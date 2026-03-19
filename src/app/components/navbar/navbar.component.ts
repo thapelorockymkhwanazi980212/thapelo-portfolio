0f53ed4517d97a0269f4d0918aed5584aed2afce
Thapelo Mkhwanazi <132280891+thapelorockymkhwanazi980212@users.noreply.github.com>
2026-03-19 15:01:11 +0200

    Modifications on the components

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -30,7 +30,7 @@ export class NavbarComponent {
     navLinks = [
       { label: 'Home', path: '/' },
       { label: 'About', path: '/about' },
-      { label: 'Projects', path: '/projects' },
+//       { label: 'Projects', path: '/projects' },
       { label: 'Contact', path: '/contact' },
     ];
 
